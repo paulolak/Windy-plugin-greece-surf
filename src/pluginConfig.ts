1e43b7a1f3b7f7cc8bf1ff5f9b441d161a1cb4b6
paulolak <pawelandrzejolak@gmail.com>
2026-01-20 14:00:59 +0200

    Create pluginConfig.ts

---
+++ src/pluginConfig.ts
@@ -0,0 +1,14 @@
+import type { ExternalPluginConfig } from '@windy/types';
+
+const config: ExternalPluginConfig = {
+    name: 'windy-plugin-greece-surf',
+    title: 'Greece Surf Map',
+    icon: '🏄‍♂️',
+    description: 'A growing map of surf spots across Greece.',
+    author: 'paulolak',
+    repository: 'https://github.com/paulolak/Windy-plugin-greece-surf',
+    desktopUI: 'rhpane',
+    mobileUI: 'fullscreen',
+};
+
+export default config;
